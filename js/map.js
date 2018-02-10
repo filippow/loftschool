a66feaebbfab69d543b3b4eaec4c8ad9d059a544
Ewgeny Filippow <filippow.ewgen@yandex.ru>
2018-02-11 01:55:14 +0300

    A lot of new changes

---
+++ js/map.js
@@ -0,0 +1,41 @@
+ymaps.ready(init);
+    var myMap;
+
+    function init(){     
+        myMap = new ymaps.Map("map", {
+            center: [59.93936595264804,30.342213997259044],
+            zoom: 11,
+            controls: []
+        });
+
+
+        myMap.behaviors.disable('scrollZoom');
+        //myMap.behaviors.disable('drag');
+
+
+
+		
+
+
+		var coords = [
+    					[59.94554327989287,30.38935262114668], 
+    					[59.91142323563909,30.50024587065841], 
+    					[59.88693161784606,30.319658102103713], 
+    					[59.97033574821672,30.315194906302924]
+					],
+
+    	myCollection = new ymaps.GeoObjectCollection({}, {
+        	iconLayout: 'default#image',
+        	iconImageHref: '../icons/map-marker.svg',
+        	iconImageSize: [46, 57],
+        	iconImageOffset: [-26, -52]
+    	});
+
+		for (var i = 0; i < coords.length; i++) {
+    		myCollection.add(new ymaps.Placemark(coords[i]));
+		}
+
+		myMap.geoObjects.add(myCollection);
+
+    
+    }
--- js/slider.js
+++ js/slider.js
@@ -1,6 +1,8 @@
 
- var acco2 = document.querySelectorAll(".menu__list"); 
 
+//Аккордеон для секции МЕНЮ
+
+ var acco2 = document.querySelectorAll(".menu__list"); 
  for (var i=0; i<acco2.length; i++) {
  	acco2[i].addEventListener('click',function(el) {
  		for (var j=0; j<acco2.length; j++) {
@@ -16,7 +18,6 @@
 //Аккордеон для секции КОМАНДА
 
 var acco = document.querySelectorAll(".team-acco__item");
-
  for (var i=0; i<acco.length; i++) {
  	acco[i].addEventListener('click',function(e) {
  		for (var j=0; j<acco.length; j++) {
@@ -27,7 +28,9 @@ var acco = document.querySelectorAll(".team-acco__item");
  	
  	});
  }
-//При клике на гамбургер, для появления меню на сером фоне
+
+
+//При клике на гамбургер, для появления меню на планшете и телефоне
 
 var gamburger = document.querySelector('.gamburger');
 gamburger.addEventListener('click',function() {
@@ -37,11 +40,12 @@ gamburger.addEventListener('click',function() {
 
 
 // При клике на меню на главном экране 
+
 var exit = document.querySelector('.exit');
 exit.addEventListener('click',function() {
 	var underMenu = document.querySelector('.under-menu');
 	underMenu.classList.add('not-visible');
-})
+});
 
 
 var link = document.querySelectorAll('.under-menu__item');
@@ -52,3 +56,81 @@ for (var i=0; i<link.length; i++) {
 		underMenu.classList.add('not-visible');
 	});
 }
+
+
+// Появление состава при нажатии на кнопку состав
+
+var consist = document.querySelector('.slider__consist');
+consist.addEventListener('click', function() {
+ 	var show = document.querySelector('.consist-info');
+ 	show.classList.toggle('consist-info--visible');
+ });
+
+
+
+//		Навигационная панель
+
+var navigat = document.querySelectorAll('.navigat__item');
+for (var i=0; i<navigat.length; i++) {
+		navigat[i].addEventListener('click', function(e) {
+			for (var j=0; j<navigat.length; j++) {
+				navigat[j].classList.remove('navigat__item--active');
+			}
+		e.currentTarget.classList.toggle('navigat__item--active');
+
+		});
+}
+
+
+
+
+
+
+// ***********************		testing
+
+/*
+
+var prev = document.querySelector('.btn--prev');
+var next = document.querySelector('.btn--next');
+var prev = document.querySelector('.btn--prev');
+var sliders = document.querySelectorAll('.test__item');
+
+var current = 0;
+
+
+
+next.addEventListener('click',function(e) {
+	e.preventDefault();
+	current++;
+	if (current>=sliders.length) {
+		current-=sliders.length;
+	}
+	show(current);
+  
+});
+	
+prev.addEventListener('click',function(e) {
+	e.preventDefault();
+	current--;
+    if (current<0) {
+		current=sliders.length-1;
+	}
+	show(current);
+});
+
+
+function show(n) {
+		for (i=0; i<sliders.length; i++) {
+			if (i!==n) {
+				sliders[i].classList.remove('test__item--current');
+			}
+			else {
+				sliders[i].classList.add('test__item--current');
+			}
+		}
+
+}
+
+*/
+
+
