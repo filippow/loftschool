53e596d3c6a2fb28b12319b897b1a02cc0c4c54c
Ewgeny Filippow <filippow.ewgen@yandex.ru>
2018-02-13 23:55:24 +0300

    After changing of the form and accordeons

--- js/slider.js
+++ js/slider.js
@@ -159,3 +159,23 @@ window.addEventListener('resize', function() {
 });
 
 
+
+
+
+
+
+//			Модальное окно отключение скролла
+/*
+
+document.addEventListener("DOMContentLoaded", function(){
+  var scrollbar = document.body.clientWidth - window.innerWidth + 'px';
+  console.log(scrollbar);
+  document.querySelector('[href="#openModal"]').addEventListener('click',function(){
+    document.body.style.overflow = 'hidden';
+    document.querySelector('#openModal').style.marginLeft = scrollbar;
+  });
+  document.querySelector('[href="#close"]').addEventListener('click',function(){
+    document.body.style.overflow = 'visible';
+    document.querySelector('#openModal').style.marginLeft = '0px';
+  });
+});*/
